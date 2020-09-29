baa298f345cdbd503cd32b8e1329eb118e0dff82
ghYoon93 <gh.yoon93@gmail.com>
2020-09-29 23:24:57 +0900

    CalculatorTest를 Junit 테스트 케이스로 변경함.

--- src/chapter02/CalculatorTest.java
+++ src/chapter02/CalculatorTest.java
@@ -1,27 +1,13 @@
 package chapter02;
 
+import org.junit.Test;
+
 public class CalculatorTest {
-	public static void main(String[] args) {
+
+	@Test
+	public void test() {
 		Calculator cal = new Calculator();
-		add(cal);
-		substract(cal);
-		multiply(cal);
-		divide(cal);
+		System.out.println(cal.add(6, 3));
 	}
 
-	private static void divide(Calculator cal) {
-		System.out.println(cal.divide(8, 4));
-	}
-
-	private static void multiply(Calculator cal) {
-		System.out.println(cal.multiply(2, 6));
-	}
-
-	private static void substract(Calculator cal) {
-		System.out.println(cal.substract(5, 4));		
-	}
-
-	private static void add(Calculator cal) {
-		System.out.println(cal.add(3, 4));		
-	}
 }
