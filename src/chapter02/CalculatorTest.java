5dd14e2eeafd0308e05c7c6e631b26a58f025200
ghYoon93 <gh.yoon93@gmail.com>
2020-09-29 23:34:48 +0900

    @Before 어노테이션을 이용해 Calculator 인스턴스 생성의 중복을 제거함.

--- src/chapter02/CalculatorTest.java
+++ src/chapter02/CalculatorTest.java
@@ -2,19 +2,23 @@ package chapter02;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+	private Calculator cal;
+	@Before
+	public void setUp() {
+		cal = new Calculator();
+	}
 
 	@Test
 	public void add() {
-		Calculator cal = new Calculator();
 		assertEquals(9, cal.add(6, 3));
 	}
 	
 	@Test
 	public void substract() {
-		Calculator cal = new Calculator();
 		assertEquals(3, cal.substract(6, 3));
 	}
 
